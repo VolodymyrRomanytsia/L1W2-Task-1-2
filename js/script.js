6516bf9ad0d6dae74ba4d015aa32054b4603284b
VolodymyrRomanytsia <romanyca@gmail.com>
2018-08-19 12:51:32 +0300

    Fixed JS

--- js/script.js
+++ js/script.js
@@ -1,20 +1,14 @@
-
-var button = document.querySelector('.button');
-button.onclick = function() {
-	var strnum = document.getElementById('arr').value.split(',');
-	var number = [];
-	for (var i = 0; i < strnum.length; i++) {
-		number.push(parseInt(strnum[i]));
+const button = document.querySelector('.button');
+button.onclick = () => {
+	const strnum = document.getElementById('arr').value.split(',');
+	let number = [];
+	for (let i = 0; i < strnum.length; i++) {
+		number.push(parseInt(strnum[i]));	
 	}
-	
-	var maxMin = function(numbers) {
-		var maxNum = Math.max.apply(null, numbers);
-		var minNum = Math.min.apply(null, numbers);
-		result = '{min: ' + minNum + ', max: ' + maxNum +'}';
-	}
-	var numbers = number;
-	console.log (maxMin(numbers));
+	const maxNum = Math.max(...number);
+	const minNum = Math.min(...number);
+	console.log ({min: minNum, max: maxNum});
+	let result = '{min: ' + minNum + ', max: ' + maxNum +'}';
 	document.getElementById("result").innerHTML = result;
 }
-
 
